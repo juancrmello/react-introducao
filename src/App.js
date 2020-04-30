56d250c9422a52580071f2c634e024a438d2ea6a
Juan Mello <juanribeirofx@gmail.com>
2020-04-30 19:53:30 -0300

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/avatar.png';
+import TechList from './components/TechList';
 
 function App() {
-  return <img width="300" src={profile} />
+  return <TechList />
 }
 
 export default App;
